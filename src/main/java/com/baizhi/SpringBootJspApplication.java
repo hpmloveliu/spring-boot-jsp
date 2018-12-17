61a8dae63acf219cd12c09cc6b75ead037345312
hpm <876288830@qq.com>
2018-12-17 21:01:31 +0800

    this is update

--- src/main/java/com/baizhi/SpringBootJspApplication.java
+++ src/main/java/com/baizhi/SpringBootJspApplication.java
@@ -8,6 +8,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @MapperScan("com.baizhi.mapper")
 public class SpringBootJspApplication {
+
+
     public static void main(String[] args) {
         //调用API
         //第一个参数为当前类的类对象，第二个参数为 main函数的参数，启动相关的参数
